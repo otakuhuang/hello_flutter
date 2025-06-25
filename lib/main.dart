7ba5e680c62cf1b5fea159af71c2f302a11647ca
otakuhuang <shyxiaopang@gmail.com>
2025-06-25 23:53:38 +0800

    refactor: change MyHomePage from StatelessWidget to StatefulWidget for enhanced state management

--- lib/main.dart
+++ lib/main.dart
@@ -61,7 +61,13 @@ class MyAppState extends ChangeNotifier {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+// private class
+class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
